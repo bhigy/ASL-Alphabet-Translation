c3204f9e1d05b89b00085a1e872bb39b18a18140
Bertrand Higy <bertrand.higy@protonmail.com>
2023-07-13 17:15:17 +0200

    final version of the demo

--- livefeed_fullbody.py
+++ livefeed_fullbody.py
@@ -1,8 +1,12 @@
 import cv2
 import imutils
 import mediapipe as mp
+import numpy as np
+import tensorflow as tf
 
 
+CATEGORIES = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "del", "nothing", "space"]
+model_asl = tf.keras.models.load_model("data/checkpoints/full.model")
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
@@ -39,7 +43,58 @@ def draw_hand_connections(img, results):
                 mpDraw.draw_landmarks(img, handLms,
                                       mpHands.HAND_CONNECTIONS)
 
-        return img
+
+
+def prepare(filepath):
+    IMG_SIZE = 48
+    new_array = cv2.resize(filepath, (IMG_SIZE, IMG_SIZE))  # resize image to match model's expected sizing
+    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+
+def recognize(image, results):
+    h, w, c = image.shape
+    if results.multi_hand_landmarks:
+        x = []
+        y = []
+        #for handLms in results.multi_hand_landmarks:
+        handLms = results.multi_hand_landmarks[0]
+        for id, lm in enumerate(handLms.landmark):
+            x.append(lm.x)
+            y.append(lm.y)
+        x_min = min(x)
+        x_max = max(x)
+        y_min = min(y)
+        y_max = max(y)
+        margin_x = (x_max - x_min) / 2
+        margin_y = (y_max - y_min) / 2
+        c0_x = round(max(x_min - margin_x, 0) * w)
+        c0_y = round(max(y_min - margin_y, 0) * h)
+        c1_x = round(min(x_max + margin_x, 1) * w)
+        c1_y = round(min(y_max + margin_y, 1) * h)
+
+        cropped = image[c0_y:c1_y, c0_x:c1_x, :]
+        frame1 = cv2.resize(cropped, (200, 200))
+        gray = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
+
+        prediction = model_asl.predict([prepare(gray)])
+        pred = (CATEGORIES[int(np.argmax(prediction[0]))])
+        return pred, (c0_x, c0_y), (c1_x, c1_y)
+    return None
+
+
+def draw_prediction(image, pred):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(image, pred, (200,100), font, 1, (0,0,0), 2, cv2.LINE_AA)
+
+
+def draw_rectangle(image, c0, c1):
+    cv2.rectangle(image, c0, c1, (0,255,0),3)
+
+
+def draw_crop(image, c0, c1):
+    width = c1[0] - c0[0]
+    height = c1[1] - c0[1]
+    image[width:, height:, :] = image[c0[0]:c1[0], c0[1]:c1[1], :]
 
 
 def main():
@@ -52,10 +107,17 @@ def main():
         success, image = cap.read()
         image = imutils.resize(image, width=500, height=500)
         results = process_image(image)
-        draw_hand_connections(image, results)
+        displayed_image = image.copy()
+        draw_hand_connections(displayed_image, results)
+        reco = recognize(image, results)
+
+        if reco is not None:
+            pred, c0, c1 = reco
+            draw_prediction(displayed_image, pred)
+            draw_rectangle(displayed_image, c0, c1)
 
         # Displaying the output
-        cv2.imshow("Hand tracker", image)
+        cv2.imshow("Hand tracker", displayed_image)
 
         # Program terminates when q key is pressed
         if cv2.waitKey(1) == ord('q'):
